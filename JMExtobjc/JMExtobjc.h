bb7281d892211b830a12d57581fe16ad32f24723
Andrey Morozov <morozov@joom.com>
2021-02-15 16:41:27 +0300

    Add-new-JMExtobjc-framework-target

---
+++ JMExtobjc/JMExtobjc.h
@@ -0,0 +1,27 @@
+//
+//  JMExtobjc.h
+//  JMExtobjc
+//
+//  Created by Andrey Morozov on 25.01.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JMExtobjc.
+FOUNDATION_EXPORT double JMExtobjcVersionNumber;
+
+//! Project version string for JMExtobjc.
+FOUNDATION_EXPORT const unsigned char JMExtobjcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JMExtobjc/PublicHeader.h>
+
+#import "EXTADT.h"
+#import "EXTConcreteProtocol.h"
+#import "EXTKeyPathCoding.h"
+#import "EXTNil.h"
+#import "EXTSafeCategory.h"
+#import "EXTScope.h"
+#import "EXTSelectorChecking.h"
+#import "EXTSynthesize.h"
+#import "NSInvocation+EXT.h"
+#import "NSMethodSignature+EXT.h"
